f07e4990120feb03c67473629bd641c0b4b51998
Lone Brotato <youngmagi13@gmail.com>
2025-08-24 18:20:42 -0500

    Level Selection initial Commit
    # Conflicts:
    #	Assets/Prefabs.meta
    #	Assets/Scripts.meta
    #	ProjectSettings/ProjectSettings.asset

---
+++ Assets/Scripts/LevelSelection/LevelSelect.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    [SerializeField]
+    private List<LevelSelectionBox> LVLSBoxes = new List<LevelSelectionBox>();
+
+    [SerializeField]
+    int EnemiesDefeated = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+
+    void Reset()
+    {
+
+
+    }
+    private void Start()
+    {
+        System.Collections.Generic.IEnumerable<LevelSelectionBox> SceneObjects = this.gameObject.GetComponentsInChildren<LevelSelectionBox>().Where(go => go.gameObject != this.gameObject);
+
+        foreach (var lvlsBox in SceneObjects)
+        {
+            LVLSBoxes.Add(lvlsBox);
+        }
+        if (LVLSBoxes.Count > 0&&LVLSBoxes.Count>EnemiesDefeated)
+         {
+            LVLSBoxes[EnemiesDefeated].SetDefault();
+         }
+        Debug.Log("LVLSBOXES:"+LVLSBoxes.Count());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        if(LVLSBoxes.Count > 0&&LVLSBoxes.Count>EnemiesDefeated) 
+        {
+            Debug.Log("Load Level:" + LVLSBoxes[EnemiesDefeated].LevelID);
+            //SceneManager.LoadScene(LVLSBoxes[EnemiesDefeated].LevelID);
+        }
+     
+    }
+}
---
+++ Assets/Scripts/LevelSelection/LevelSelectionBox.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class LevelSelectionBox : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    private Image CharacterPortrait;
+    private TextMeshProUGUI CharacterNameText;
+    [SerializeField]
+    private  Sprite DefaultPortrait;
+    [SerializeField]
+    private Sprite LossPortrait;
+    [SerializeField]
+    private string CharacterTitle;
+    [SerializeField]
+    public int LevelID;
+
+
+
+
+    void Awake()
+    {
+        System.Collections.Generic.IEnumerable<Image> SceneObjects = this.gameObject.GetComponentsInChildren<Image>().Where(go => go.gameObject != this.gameObject);
+
+        foreach (Image image in SceneObjects)
+        {
+            if(image!=this.gameObject.GetComponent<Image>())
+            {
+                CharacterPortrait = image;
+            }
+        }
+
+
+       
+        CharacterNameText=GetComponentInChildren<TextMeshProUGUI>();
+        Debug.Log("BEEP");
+        
+    }
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDefault()
+    {
+        UpdateCharacter(DefaultPortrait, CharacterTitle);
+    }
+
+    public void UpdateCharacter(Sprite portrait, string CharacterTitle)
+    {
+        CharacterPortrait.sprite=portrait;
+        CharacterNameText.text=CharacterTitle;
+    }
+
+}
