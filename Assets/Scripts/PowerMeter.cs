f056607c192c4b917313d88c54a29975b9ed3b23
Zeldatechie <jesrey4@outlook.com>
2025-08-29 22:06:10 -0400

    Created power meter visual

--- Assets/Scripts/PowerMeter.cs
+++ Assets/Scripts/PowerMeter.cs
@@ -3,13 +3,25 @@ using NUnit.Framework.Interfaces;
 using System;
 using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerMeter : MonoBehaviour
 {
     [SerializeField] public bool isCharging { get; private set; }
-
     [SerializeField]public float currentPower { get; private set; }
+
     Tween currentChargingTween;
+    [SerializeField]Image currentChargingImage;
+    [SerializeField] GameObject powerMeterContainer;
+
+    public void Update()
+    {
+        if(powerMeterContainer.activeSelf)
+        {
+            Debug.Log($"CurrentPower: {currentPower}");
+            currentChargingImage.fillAmount = currentPower;
+        }
+    }
 
     public void StartCharging()
     {
@@ -17,6 +29,7 @@ public class PowerMeter : MonoBehaviour
         {
             Debug.LogError("Tried to start charging when meter was already charging");
         }
+        powerMeterContainer.SetActive(true);
         isCharging = true;
         currentPower = 0;
         currentChargingTween = DOTween.To(() => currentPower, tweenedValue => currentPower = tweenedValue, 1f, 2f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Linear).SetDelay(.1f);
@@ -28,6 +41,7 @@ public class PowerMeter : MonoBehaviour
         {
             Debug.LogError("Tried to stop charging when meter wasn't already charging");
         }
+        powerMeterContainer.SetActive(false);
         isCharging = false;
         currentChargingTween.Kill();
         return currentPower;
